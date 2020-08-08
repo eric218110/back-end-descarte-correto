5017d50ede3d8fa50194a97090380f38e48ba61c
Eric Silva Mendes <ericsilvaccp@gmail.com>
2020-08-08 13:42:00 -0300

    test: ensure DbAddAccount return account if sucess

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -100,4 +100,20 @@ describe('DbAddAccount', () => {
     const promisse = sut.add(accountData)
     await expect(promisse).rejects.toThrow()
   })
+
+  test('should return an account on sucess', async () => {
+    const { sut } = makeSut()
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email@mail.com',
+      password: 'valid_password'
+    }
+    const account = await sut.add(accountData)
+    expect(account).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_email@mail.com',
+      password: 'hashed_password'
+    })
+  })
 })
