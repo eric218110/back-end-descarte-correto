72b6e04dd640cbc4caa54a5d85163c3819614318
Eric Silva Mendes <ericsilvaccp@gmail.com>
2020-08-07 22:46:21 -0300

    feat: ensure signUpController return 400 if password confitmation fails

--- src/presentation/controllers/SignUp.ts
+++ src/presentation/controllers/SignUp.ts
@@ -19,7 +19,12 @@ export class SignUpController implements IController {
         }
       }
 
+      if (httpRequest.body.password !== httpRequest.body.passwordConfirmation) {
+        return badRequest(new InvalidParamError('passwordConfirmation'))
+      }
+
       const isValid = this.emailValidator.isValid(httpRequest.body.email)
+
       if (!isValid) {
         return badRequest(new InvalidParamError('email'))
       }
