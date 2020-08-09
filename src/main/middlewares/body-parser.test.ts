62ba4e98b442495da23b291932637a07a11f278e
Eric Silva Mendes <ericsilvaccp@gmail.com>
2020-08-09 20:13:08 -0300

    test: ensure body parser request as JSON

---
+++ src/main/middlewares/body-parser.test.ts
@@ -0,0 +1,15 @@
+import request from 'supertest'
+import app from '../config/app'
+import { Request, Response } from 'express'
+
+describe('Body Parser Middleware', () => {
+  test('Should parser body request as JSON', async () => {
+    app.post('/test_body_parser', (req: Request, res: Response) => {
+      res.send(req.body)
+    })
+    await request(app)
+      .post('/test_body_parser')
+      .send({ name: 'Eric Silva' })
+      .expect({ name: 'Eric Silva' })
+  })
+})
