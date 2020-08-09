ec56b7a4454b0dd021a971a2600c194d3d33f968
Eric Silva Mendes <ericsilvaccp@gmail.com>
2020-08-09 20:46:27 -0300

    test: ensure return default content type as JSON

---
+++ src/main/middlewares/content-type-json/content-type-json.test.ts
@@ -0,0 +1,14 @@
+import request from 'supertest'
+import app from '../../config/app'
+import { Request, Response } from 'express'
+
+describe('Content type JSON Middleware', () => {
+  test('Should return content type default as JSON', async () => {
+    app.get('/test_content_type', (req: Request, res: Response) => {
+      res.json({ nema: 'er' })
+    })
+    await request(app)
+      .get('/test_content_type')
+      .expect('content-type', /json/)
+  })
+})
