cc0ba4f9caa74d45115312b16920b6bfab359988
Eric Silva Mendes <ericsilvaccp@gmail.com>
2020-08-09 20:45:24 -0300

    refactor: change type request post as get

--- src/main/middlewares/cors/cors.test.ts
+++ src/main/middlewares/cors/cors.test.ts
@@ -8,7 +8,7 @@ describe('CORS Middleware', () => {
       res.send()
     })
     await request(app)
-      .post('/test_cors')
+      .get('/test_cors')
       .expect('access-control-allow-origin', '*')
       .expect('access-control-allow-methods', '*')
       .expect('access-control-allow-headers', '*')
