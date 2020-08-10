5be65e7e33f432090c73d9989b23a658ced7fe51
Eric Silva Mendes <ericsilvaccp@gmail.com>
2020-08-10 19:10:15 -0300

    test: ensure LoggerControllerDecorator call if controller correct

---
+++ src/main/decorators/logger.spec.ts
@@ -0,0 +1,46 @@
+import { LoggerControllerDecorator } from './logger'
+import { HttpRequest, Controller, HttpResponse } from '../../presentation/protocols'
+
+interface SutTypes {
+  sut: LoggerControllerDecorator
+  controllerStub: Controller
+}
+
+const makeControllerStub = (): Controller => {
+  class ControllerStub implements Controller {
+    async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+      const httpResponse: HttpResponse = {
+        body: {
+          name: 'Eric Silva',
+          email: 'ericsilvaccp@gmail.com'
+        },
+        statusCode: 200
+      }
+      return new Promise(resolve => resolve(httpResponse))
+    }
+  }
+  return new ControllerStub()
+}
+
+const makeSut = (): SutTypes => {
+  const controllerStub = makeControllerStub()
+  const sut = new LoggerControllerDecorator(controllerStub)
+  return { sut, controllerStub }
+}
+
+describe('Logger Controller Decorator', () => {
+  test('should LoggerControllerDecorator call handler', async () => {
+    const { sut, controllerStub } = makeSut()
+    const handleSpy = jest.spyOn(controllerStub, 'handle')
+    const httpRequest: HttpRequest = {
+      body: {
+        email: 'any_mail@mail.com',
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    await sut.handle(httpRequest)
+    expect(handleSpy).toHaveBeenCalledWith(httpRequest)
+  })
+})
