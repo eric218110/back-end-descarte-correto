a7ea44ec605544802aa77991b0268ef14e32e16c
Eric Silva Mendes <ericsilvaccp@gmail.com>
2020-08-12 00:50:52 -0300

    test: move create sut to a factory

--- src/presentation/helper/validation/required-field-validator.spec.ts
+++ src/presentation/helper/validation/required-field-validator.spec.ts
@@ -1,15 +1,19 @@
 import { RequireFieldValidator } from './required-field-validator'
 import { MissingParamsError } from '../../errors'
 
+const makeSut = (): RequireFieldValidator => {
+  return new RequireFieldValidator('any_field')
+}
+
 describe('RequiredFieldValidation', () => {
   test('should return a MissingParamError if validation fails', () => {
-    const sut = new RequireFieldValidator('any_field')
+    const sut = makeSut()
     const isError = sut.isValid({ name: 'invalid_name' })
     expect(isError).toEqual(new MissingParamsError('any_field'))
   })
 
   test('should not return a MissingParamError success', () => {
-    const sut = new RequireFieldValidator('any_field')
+    const sut = makeSut()
     const isError = sut.isValid({ any_field: 'any_field_name' })
     expect(isError).toBeFalsy()
   })
