a38da3edd730b95b12def2c4b7214ef8c6585728
Eric Silva Mendes <ericsilvaccp@gmail.com>
2020-08-12 00:49:31 -0300

    test: ensure RequireFieldValidation not return if MissingParamError success

--- src/presentation/helper/validation/required-field-validator.spec.ts
+++ src/presentation/helper/validation/required-field-validator.spec.ts
@@ -7,4 +7,10 @@ describe('RequiredFieldValidation', () => {
     const isError = sut.isValid({ name: 'invalid_name' })
     expect(isError).toEqual(new MissingParamsError('any_field'))
   })
+
+  test('should not return a MissingParamError success', () => {
+    const sut = new RequireFieldValidator('any_field')
+    const isError = sut.isValid({ any_field: 'any_field_name' })
+    expect(isError).toBeFalsy()
+  })
 })
