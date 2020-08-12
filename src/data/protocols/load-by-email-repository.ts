9cb17811d9107b68971aef78412a25292b521810
Eric Silva Mendes <ericsilvaccp@gmail.com>
2020-08-12 15:24:42 -0300

    feat: ensure DbAuthenticate call LoadByEmailRepository with correct email

---
+++ src/data/protocols/load-by-email-repository.ts
@@ -0,0 +1,5 @@
+import { AccountModel } from '../../domain/models/account'
+
+export interface LoadByEmailRepository {
+  loadWithEmail: (email: string) => Promise<AccountModel>
+}
---
+++ src/data/usecases/authentication/db-authentication.ts
@@ -0,0 +1,13 @@
+import { Authentication, AuthenticationModel } from '../../../domain/usecases/authentication'
+import { LoadByEmailRepository } from '../../protocols/load-by-email-repository'
+
+export class DbAuthentication implements Authentication {
+  constructor (
+    private readonly loadByEmailRepository: LoadByEmailRepository
+  ) {}
+
+  async auth (authentication: AuthenticationModel): Promise<string> {
+    await this.loadByEmailRepository.loadWithEmail(authentication.email)
+    return ''
+  }
+}
