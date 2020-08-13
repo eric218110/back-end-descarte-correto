32817e0366edc356c6391bfedf365f81e281912e
Eric Silva Mendes <ericsilvaccp@gmail.com>
2020-08-13 15:57:37 -0300

    feat: ensure JwtAdapter call jsonwebtoken sign with values correct

---
+++ src/infra/criptography/jwt-adapter/jwt-adapter.ts
@@ -0,0 +1,13 @@
+import jsonwebtoken from 'jsonwebtoken'
+import { Encrypter } from '../../../data/protocols/criptography/encripter'
+
+export class JwtAdapter implements Encrypter {
+  constructor (
+    private readonly secret: string
+  ) {}
+
+  async encript (value: string): Promise<string> {
+    await jsonwebtoken.sign({ id: value }, this.secret)
+    return null
+  }
+}
