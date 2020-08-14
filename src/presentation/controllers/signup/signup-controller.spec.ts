8e713080988817951066bb9c4995363b28599f24
Eric Silva Mendes <ericsilvaccp@gmail.com>
2020-08-14 00:37:09 -0300

    test: ensure Authentication return 200 and accessToken in body if valid data is provided

--- src/presentation/controllers/signup/signup-controller.spec.ts
+++ src/presentation/controllers/signup/signup-controller.spec.ts
@@ -96,10 +96,12 @@ describe('Signup Controller', () => {
     expect(httpResponse).toEqual(serverError(new ServerError(null)))
   })
 
-  test('Should return 200 if valid data is provided', async () => {
+  test('Should return 200 and accessToken in body if valid data is provided', async () => {
     const { sut } = makeSut()
     const httpResponse = await sut.handle(makeFakeRequest())
-    expect(httpResponse).toEqual(ok(makeFakeAccount()))
+    expect(httpResponse).toEqual(ok({
+      accessToken: 'valid_token'
+    }))
   })
 
   test('Should call Validator with correct value', async () => {
