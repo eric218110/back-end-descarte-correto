191f40dc4c93b8876700d433888636b85da54014
Eric Silva Mendes <ericsilvaccp@gmail.com>
2020-08-16 00:47:21 -0300

    refactor: move folders controller login and signup to a folder account

--- src/main/factories/controllers/login/login-controller-factory.ts
+++ src/main/factories/controllers/login/login-controller-factory.ts
@@ -1,5 +1,5 @@
 import { Controller } from '@presentation/protocols'
-import { LoginController } from '@presentation/controllers/login/login-controller'
+import { LoginController } from '@presentation/controllers/account/login/login-controller'
 import { makeLoginValidator } from './login-validator-factory'
 import { makeDbAuthentication } from '@main/factories/usecases/authentication/db-authentication-factory'
 import { makeLoggerControllerDecorator } from '@main/factories/decorators/logger-controller/logger-controller-decorator-factory'
--- src/main/factories/controllers/signup/signup-controller-factory.ts
+++ src/main/factories/controllers/signup/signup-controller-factory.ts
@@ -1,4 +1,4 @@
-import { SignUpController } from '@presentation/controllers/signup/signup-controller'
+import { SignUpController } from '@presentation/controllers/account/signup/signup-controller'
 import { Controller } from '@presentation/protocols'
 import { makeSignUpValidator } from './signup-validator-factory'
 import { makeDbAddAccount } from '@main/factories/usecases/add-account/db-add-account-factory'
