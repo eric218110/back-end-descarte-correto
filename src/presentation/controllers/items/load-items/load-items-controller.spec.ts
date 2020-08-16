d7f61c155468f6c0eaa2518e736a97678743cc4d
Eric Silva Mendes <ericsilvaccp@gmail.com>
2020-08-16 01:32:51 -0300

    test: ensure LoadItemsController call LoadItems correctly

---
+++ src/presentation/controllers/items/load-items/load-items-controller.spec.ts
@@ -0,0 +1,43 @@
+import { LoadItemsController } from './load-items-controller'
+import { LoadItems, LoadItemsModel } from './load-items-controller-protocols'
+
+type SutType = {
+  sut: LoadItemsController
+  loadItemsStub: LoadItems
+  fakeLoadItems: LoadItemsModel[]
+}
+
+const makeLoadItemsFake = (): LoadItemsModel[] => ([
+  { image: 'http://any_image_1.com', title: 'any_title_1' },
+  { image: 'http://any_image_2.com', title: 'any_title_2' },
+  { image: 'http://any_image_3.com', title: 'any_title_3' }
+])
+
+const makeLoadItemsStub = (): LoadItems => {
+  class LoadItemsStub implements LoadItems {
+    async load (): Promise<LoadItemsModel[]> {
+      return new Promise(resolve => resolve(makeLoadItemsFake()))
+    }
+  }
+  return new LoadItemsStub()
+}
+
+const makeSut = (): SutType => {
+  const loadItemsStub = makeLoadItemsStub()
+  const sut = new LoadItemsController(loadItemsStub)
+  const fakeLoadItems = makeLoadItemsFake()
+  return {
+    sut,
+    loadItemsStub,
+    fakeLoadItems
+  }
+}
+
+describe('LoadItemsController', () => {
+  test('should call LoadItems', async () => {
+    const { sut, loadItemsStub } = makeSut()
+    const loadSpy = jest.spyOn(loadItemsStub, 'load')
+    await sut.handle({})
+    expect(loadSpy).toBeCalled()
+  })
+})
