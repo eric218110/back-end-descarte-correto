1b129f040be2c56bac3a3af35b1241e81c7c3ddc
Eric Silva Mendes <ericsilvaccp@gmail.com>
2020-08-16 01:50:10 -0300

    test: ensure LoadItemsController return 204 if LoadItems returns null

--- src/presentation/controllers/items/load-items/load-items-controller.spec.ts
+++ src/presentation/controllers/items/load-items/load-items-controller.spec.ts
@@ -1,4 +1,5 @@
 import { LoadItemsController } from './load-items-controller'
+import { noContent } from '@presentation/helper/http/http-helper'
 import { LoadItems, LoadItemsModel } from './load-items-controller-protocols'
 
 type SutType = {
@@ -39,5 +40,13 @@ describe('LoadItemsController', () => {
     const loadSpy = jest.spyOn(loadItemsStub, 'load')
     await sut.handle({})
     expect(loadSpy).toBeCalled()
+  })
+
+  test('should return if LoadItems returns null', async () => {
+    const { sut, loadItemsStub } = makeSut()
+    jest.spyOn(loadItemsStub, 'load')
+      .mockReturnValueOnce(new Promise(resolve => resolve(null)))
+    const response = await sut.handle({})
+    expect(response).toEqual(noContent())
   })
 })
