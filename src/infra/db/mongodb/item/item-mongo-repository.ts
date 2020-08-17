aa8c877231ef0cc01d2666cf59862c3181973bb9
Eric Silva Mendes <ericsilvaccp@gmail.com>
2020-08-17 19:34:55 -0300

    feat: ensure ItemMongoRepositoty return all items on sucess

---
+++ src/infra/db/mongodb/item/item-mongo-repository.ts
@@ -0,0 +1,11 @@
+import { LoadItemsRepository } from '@data/protocols/items/load-items-repository'
+import { LoadItemsModel } from '@domain/usecases/load-items'
+import { MongoHelper } from '../helpers/mongo-helper'
+
+export class ItemMongoRepositoty implements LoadItemsRepository {
+  async loadAllItems (): Promise<LoadItemsModel[]> {
+    const itemsCollection = await MongoHelper.getCollection('items')
+    const items = await itemsCollection.find().toArray()
+    return items
+  }
+}
