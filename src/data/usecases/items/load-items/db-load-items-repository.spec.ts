2be2c34141bf045f42b75322549c66643c211635
Eric Silva Mendes <ericsilvaccp@gmail.com>
2020-08-17 12:20:04 -0300

    test: ensure DbLoadItemsRepository call LoadItemRepositoty correctly

---
+++ src/data/usecases/items/load-items/db-load-items-repository.spec.ts
@@ -0,0 +1,44 @@
+import { DbLoadItemsRepository } from './db-load-items-repository'
+import { LoadItemsRepository } from '@data/protocols/items/load-items-repository'
+import { LoadItemsModel } from '@domain/usecases/load-items'
+
+type SutTypes = {
+  sut: DbLoadItemsRepository
+  loadItemsRepositoryStub: LoadItemsRepository
+  itemsFake: LoadItemsModel[]
+}
+
+const makeLoadItemsFake = (): LoadItemsModel[] => ([
+  { image: 'http://any_image_1.com', title: 'any_title_1' },
+  { image: 'http://any_image_2.com', title: 'any_title_2' },
+  { image: 'http://any_image_3.com', title: 'any_title_3' }
+])
+
+const makeLoadItemsRepositoryStub = (): LoadItemsRepository => {
+  class LoadItemsRepositoryStub implements LoadItemsRepository {
+    async loadAllItems (): Promise<LoadItemsModel[]> {
+      return new Promise(resolve => resolve(makeLoadItemsFake()))
+    }
+  }
+  return new LoadItemsRepositoryStub()
+}
+
+const makeSut = (): SutTypes => {
+  const loadItemsRepositoryStub = makeLoadItemsRepositoryStub()
+  const sut = new DbLoadItemsRepository(loadItemsRepositoryStub)
+  const itemsFake = makeLoadItemsFake()
+  return {
+    sut,
+    loadItemsRepositoryStub,
+    itemsFake
+  }
+}
+
+describe('DbLoadItemsRepository', () => {
+  test('should call LoadItemRepositoty correctly', async () => {
+    const { sut, loadItemsRepositoryStub } = makeSut()
+    const loadAllItemsSpy = jest.spyOn(loadItemsRepositoryStub, 'loadAllItems')
+    await sut.load()
+    expect(loadAllItemsSpy).toBeCalled()
+  })
+})
