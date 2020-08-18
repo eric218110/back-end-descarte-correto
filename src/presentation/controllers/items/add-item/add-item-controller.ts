bdbad5262591c8ff9e92c120ee3624f847703d82
Eric Silva Mendes <ericsilvaccp@gmail.com>
2020-08-18 04:33:45 -0300

    feat: ensure AddItemController calls Validator with values correctly

--- src/presentation/controllers/items/add-item/add-item-controller.ts
+++ src/presentation/controllers/items/add-item/add-item-controller.ts
@@ -2,17 +2,20 @@ import {
   Controller,
   HttpRequest,
   HttpResponse,
-  AddItem
-} from './add-items-controller-protocols'
+  AddItem,
+  Validator
+} from './add-item-controller-protocols'
 import { serverError, onCreated } from '@presentation/helper/http/http-helper'
 
 export class AddItemController implements Controller {
   constructor (
-    private readonly addItem: AddItem
+    private readonly addItem: AddItem,
+    private readonly validator: Validator
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
+      this.validator.isValid(httpRequest.body)
       const { image, title } = httpRequest.body
       const item = await this.addItem.add({ image, title })
       return onCreated(item)
