ccc7b5ef5f73ef531af8024f56cf53dcf21ad9e6
Eric Silva Mendes <ericsilvaccp@gmail.com>
2020-08-18 14:27:34 -0300

    feat: ensure DbAddItemRepository return item if AddtemRepository success

--- src/data/usecases/items/add-items/db-add-item-repository.ts
+++ src/data/usecases/items/add-items/db-add-item-repository.ts
@@ -2,13 +2,13 @@ import { AddItem, AddItemModel } from '@domain/usecases/add-item'
 import { ItemModel } from '@domain/models/item'
 import { AddItemRepository } from '@data/protocols/items/add-items-repository'
 
-export class DbAddtemRepository implements AddItem {
+export class DbAddItemRepository implements AddItem {
   constructor (
     private readonly addItemRepository: AddItemRepository
   ) {}
 
   async add (item: AddItemModel): Promise<ItemModel> {
-    await this.addItemRepository.addNewItem(item)
-    return null
+    const newItem = await this.addItemRepository.addNewItem(item)
+    return newItem
   }
 }
