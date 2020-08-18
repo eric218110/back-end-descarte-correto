1d552f41bd41f87ae1ed63637474de93e10f4bad
Eric Silva Mendes <ericsilvaccp@gmail.com>
2020-08-18 14:54:55 -0300

    refactor: changer name folder

--- src/main/factories/usecases/add-account/db-add-account-factory.ts
+++ src/main/factories/usecases/add-account/db-add-account-factory.ts
@@ -1,7 +1,7 @@
 import { AccountMongoRepository } from '@infra/db/mongodb/account/account-mongo-repository'
 import { BCryptAdapter } from '@infra/criptography/brcypt-adapter/brcypt-adapter'
 import env from '@main/config/env'
-import { DbAddAccount } from '@data/usecases/add-account/db-add-account'
+import { DbAddAccount } from '@data/usecases/account/db-add-account'
 import { AddAccount } from '@domain/usecases/add-account'
 
 export const makeDbAddAccount = (): AddAccount => {
