a3a305f913d155f3f5f911308632146314a47202
Eric Silva Mendes <ericsilvaccp@gmail.com>
2020-08-18 23:54:36 -0300

    refactor: rename import ItemMongoRepository in factory controller

--- src/main/factories/controllers/item/load-items-controller.ts
+++ src/main/factories/controllers/item/load-items-controller.ts
@@ -2,10 +2,10 @@ import { Controller } from '@presentation/protocols'
 import { LoadItemsController } from '@presentation/controllers/items/load-items/load-items-controller'
 import { makeLoggerControllerDecorator } from '@main/factories/decorators/logger-controller/logger-controller-decorator-factory'
 import { DbLoadItemsRepository } from '@data/usecases/items/load-items/db-load-items-repository'
-import { ItemMongoRepositoty } from '@infra/db/mongodb/item/item-mongo-repository'
+import { ItemMongoRepository } from '@infra/db/mongodb/item/item-mongo-repository'
 
 export const makeLoadItemController = (): Controller => {
-  const loadItemsMongoRepository = new ItemMongoRepositoty()
+  const loadItemsMongoRepository = new ItemMongoRepository()
   const dbLoadItems = new DbLoadItemsRepository(loadItemsMongoRepository)
   const loadItemsController = new LoadItemsController(dbLoadItems)
   return makeLoggerControllerDecorator(loadItemsController)
