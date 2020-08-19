4154a6c0272021cf585f68acf66b744dbd417089
Eric Silva Mendes <ericsilvaccp@gmail.com>
2020-08-19 20:57:52 -0300

    feat: ensure Add item controller return 403 on add item without accessToken

---
+++ src/main/factories/middlewares/auth/auth-middleware-factory.ts
@@ -0,0 +1,7 @@
+import { Middleware } from '@presentation/protocols'
+import { AuthMiddleware } from '@presentation/middlewares/auth/auth-middleware'
+import { makeDbLoadAccountByToken } from '@main/factories/usecases/account/load-account-by-token/db-load-account-by-token-factory'
+
+export const makeAuthMiddleware = (role?: string): Middleware => {
+  return new AuthMiddleware(makeDbLoadAccountByToken(), role)
+}
---
+++ src/main/factories/usecases/account/load-account-by-token/db-load-account-by-token-factory.ts
@@ -0,0 +1,11 @@
+import { LoadAccountByToken } from '@domain/usecases/load-accout-by-token'
+import { DbLoadAccountByToken } from '@data/usecases/load-account-by-token/db-load-account-by-token'
+import { JwtAdapter } from '@infra/criptography/jwt-adapter/jwt-adapter'
+import env from '@main/config/env'
+import { AccountMongoRepository } from '@infra/db/mongodb/account/account-mongo-repository'
+
+export const makeDbLoadAccountByToken = (): LoadAccountByToken => {
+  const jwtAdapter = new JwtAdapter(env.jwt_secret)
+  const loadAccountByTokenRepository = new AccountMongoRepository()
+  return new DbLoadAccountByToken(jwtAdapter, loadAccountByTokenRepository)
+}
