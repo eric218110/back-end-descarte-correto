bc4f9501bfca49dab73b881cd5cfe45fe55f49cc
Eric Silva Mendes <ericsilvaccp@gmail.com>
2020-08-19 14:48:33 -0300

    refactor: move AuthMiddleware dependencies to a protocols file

---
+++ src/presentation/middlewares/auth/auth-middleware-protocols.ts
@@ -0,0 +1,5 @@
+export * from '@presentation/protocols'
+export * from '@domain/usecases/load-accout-by-token'
+export * from '@presentation/helper/http/http-helper'
+export * from '@presentation/errors/access-denied-error'
+export * from '@domain/models/account'
