8c3669bf865bda1d28f9ecf27f2c69ad977a1150
Eric Silva Mendes <ericsilvaccp@gmail.com>
2020-08-20 20:08:15 -0300

    feat: ensure UploadImageMiddleware return image url if UploadImage success

--- src/presentation/middlewares/upload/upload-image-middleware.ts
+++ src/presentation/middlewares/upload/upload-image-middleware.ts
@@ -3,7 +3,8 @@ import {
   HttpRequest,
   HttpResponse,
   UploadImage,
-  serverError
+  serverError,
+  ok
 } from './upload-image-middleware-protocols'
 
 export class UploadImageMiddleware implements Middleware {
@@ -13,8 +14,8 @@ export class UploadImageMiddleware implements Middleware {
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      await this.uploadImage.upload(httpRequest.file)
-      return null
+      const imageUrl = await this.uploadImage.upload(httpRequest.file)
+      return ok({ imageUrl })
     } catch (error) {
       return serverError(error)
     }
