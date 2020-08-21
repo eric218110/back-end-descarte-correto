4eea993e373717e7c44994200825f17be8cc248d
Eric Silva Mendes <ericsilvaccp@gmail.com>
2020-08-21 03:05:01 -0300

    refactor: rename error

--- src/presentation/errors/email-in-use-error.ts
+++ src/presentation/errors/email-in-use-error.ts
@@ -1,6 +1,6 @@
 export class EmailInUserError extends Error {
   constructor () {
     super('This received email is already in use')
-    this.name = 'Forbidden'
+    this.name = 'EmailInUserError'
   }
 }
