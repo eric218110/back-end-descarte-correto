d73225165ac4f3b90834d08e24f1d3dd9e698e07
Eric Silva Mendes <ericsilvaccp@gmail.com>
2020-08-22 01:18:27 -0300

    fix: correct response mapping

--- src/main/adapters/express/express-middleware-adapter.ts
+++ src/main/adapters/express/express-middleware-adapter.ts
@@ -9,7 +9,7 @@ export const adaptMiddleware = (middleware: Middleware) => {
     }
     const httpResponse = await middleware.handle(httpRequest)
     if (httpResponse.statusCode === 200) {
-      Object.assign(req, httpResponse.body)
+      Object.assign(req.body, httpResponse.body)
       next()
     } else {
       res.status(httpResponse.statusCode).json({
