bb776cd44ce7ee4e7d612e0fc2b10816794cc728
Eric Silva Mendes <ericsilvaccp@gmail.com>
2020-08-23 02:37:54 -0300

    refactor: changer mapping

--- src/main/adapters/express/express-route-adapter.ts
+++ src/main/adapters/express/express-route-adapter.ts
@@ -3,9 +3,20 @@ import { Request, Response } from 'express'
 
 export const addpterRoute = (controller: Controller) => {
   return async (request: Request, response: Response) => {
+    if (request.file) {
+
+    }
     const httpRequest: HttpRequest = {
       body: request.body,
-      headers: request.headers
+      headers: request.headers,
+      file: (request.file) ? {
+        request: request.file,
+        response: null
+      }
+        : {
+          request: null,
+          response: null
+        }
     }
     const { body, statusCode } = await controller.handle(httpRequest)
     if (statusCode >= 200 && statusCode <= 299) {
