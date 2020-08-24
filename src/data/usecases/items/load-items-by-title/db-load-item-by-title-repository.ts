e3d68ab0ce5e2f2cc2dd953a24466849ece9e323
Eric Silva Mendes <ericsilvaccp@gmail.com>
2020-08-23 22:26:07 -0300

    feat: ensure DbLoadItemByTitleRepository call LoadItemByToken correctly

---
+++ src/data/usecases/items/load-items-by-title/db-load-item-by-title-repository.ts
@@ -0,0 +1,14 @@
+import { LoadItemByTitleRepository } from '@data/protocols/data/items/load-item-by-title-repository'
+import { ItemModel } from '../add-items/db-add-items-protocols'
+import { LoadItemByTitle } from '@domain/usecases/item/load-item-by-title'
+
+export class DbLoadItemByTitleRepository implements LoadItemByTitle {
+  constructor (
+    private readonly loadItemByTitleRepository: LoadItemByTitleRepository
+  ) {}
+
+  async load (title: any): Promise<ItemModel> {
+    await this.loadItemByTitleRepository.loadByTitle(title)
+    return new Promise(resolve => resolve(null))
+  }
+}
