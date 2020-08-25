50ecdc9348410e2f6f980fa5f665e4252062490d
Eric Silva Mendes <ericsilvaccp@gmail.com>
2020-08-24 21:00:36 -0300

    test: ensure SaveImage call ImageFileUploader with correct values in mode online

--- src/data/usecases/upload/upload-image/save-image.spec.ts
+++ src/data/usecases/upload/upload-image/save-image.spec.ts
@@ -14,16 +14,16 @@ const makeFileFake = (): FileProps => ({
 
 const makeImageFileUploaderStub = (): ImageFileUploader => {
   class ImageFileUploaderStub implements ImageFileUploader {
-    async imageUpload (fileImage: FileProps): Promise<void> {
+    async imageUpload (fileImage: FileProps, storageType: 'LOCAL' | 'ONLINE'): Promise<void> {
       return new Promise(resolve => resolve())
     }
   }
   return new ImageFileUploaderStub()
 }
 
-const makeSut = (): SutTypes => {
+const makeSut = (env: 'LOCAL' | 'ONLINE'): SutTypes => {
   const imageFileUploaderStub = makeImageFileUploaderStub()
-  const sut = new SaveImage(imageFileUploaderStub, 'LOCAL')
+  const sut = new SaveImage(imageFileUploaderStub, env)
   const fileFake = makeFileFake()
   return {
     imageFileUploaderStub,
@@ -34,14 +34,21 @@ const makeSut = (): SutTypes => {
 
 describe('SaveImage', () => {
   test('should call ImageFileUploader with correct values in mode development', async () => {
-    const { sut, imageFileUploaderStub, fileFake } = makeSut()
+    const { sut, imageFileUploaderStub, fileFake } = makeSut('LOCAL')
     const imageUploadSpy = jest.spyOn(imageFileUploaderStub, 'imageUpload')
     await sut.upload(fileFake)
     expect(imageUploadSpy).toHaveBeenCalledWith(fileFake, 'LOCAL')
   })
 
+  test('should call ImageFileUploader with correct values in online development', async () => {
+    const { sut, imageFileUploaderStub, fileFake } = makeSut('ONLINE')
+    const imageUploadSpy = jest.spyOn(imageFileUploaderStub, 'imageUpload')
+    await sut.upload(fileFake)
+    expect(imageUploadSpy).toHaveBeenCalledWith(fileFake, 'ONLINE')
+  })
+
   test('should throws if ImageFileUploader throws', async () => {
-    const { sut, imageFileUploaderStub, fileFake } = makeSut()
+    const { sut, imageFileUploaderStub, fileFake } = makeSut('LOCAL')
     jest.spyOn(imageFileUploaderStub, 'imageUpload')
       .mockReturnValueOnce(
         new Promise((resolve, reject) => reject(new Error()))
