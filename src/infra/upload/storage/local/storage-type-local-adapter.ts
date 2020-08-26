748c727fad55085b4dc0bc12ae777d84891645a0
Eric Silva Mendes <ericsilvaccp@gmail.com>
2020-08-26 12:14:36 -0300

    feat: ensure StorageTypeLocalAdapter return throws type error if file is undefined

--- src/infra/upload/storage/local/storage-type-local-adapter.ts
+++ src/infra/upload/storage/local/storage-type-local-adapter.ts
@@ -6,6 +6,9 @@ export class StorageTypeLocalAdapter implements SavedFileStorage {
   ) {}
 
   async saveFile (request: any): Promise<string> {
+    if (request === undefined) {
+      throw TypeError('file is required')
+    }
     const filename: string = request.file.filename
     const pathUrlFile = `${this.staticPathFilesUrl}${filename}`
     return pathUrlFile
