bf9d22ba0293fe2f3b60634c8b231aab9afb606b
Eric Silva Mendes <ericsilvaccp@gmail.com>
2020-08-26 02:13:30 -0300

    test: ensure SavedImageStorage call save with correct values

---
+++ src/data/usecases/upload/storage/saved-image-storage.spec.ts
@@ -0,0 +1,46 @@
+import { SavedImageStorage } from './saved-image-storage'
+import { SavedFileStorage } from '@data/protocols/upload/storage/saved-file-storage'
+import { HttpRequest } from '@presentation/protocols'
+
+type SutTypes = {
+  sut: SavedImageStorage
+  savedFileStorage: SavedFileStorage
+}
+
+const makeSavedFileStorageStub = (): SavedFileStorage => {
+  class SavedFileStorageStub implements SavedFileStorage {
+    async saveFile (request: any, fileName: string): Promise<void> {
+      return new Promise(resolve => resolve())
+    }
+  }
+  return new SavedFileStorageStub()
+}
+
+const makeSut = (): SutTypes => {
+  const savedFileStorage = makeSavedFileStorageStub()
+  const sut = new SavedImageStorage(savedFileStorage)
+  return {
+    savedFileStorage,
+    sut
+  }
+}
+
+const makeFakeRequest = (): HttpRequest => ({
+  file: {
+    request: {
+      file: {
+        orinalname: 'any_original_name'
+      }
+    },
+    response: {}
+  }
+})
+
+describe('SavedImageStorage', () => {
+  test('should call save with correct values', async () => {
+    const { sut, savedFileStorage } = makeSut()
+    const spySaveFile = jest.spyOn(savedFileStorage, 'saveFile')
+    await sut.save(makeFakeRequest(), 'any_name')
+    expect(spySaveFile).toBeCalledWith(makeFakeRequest(), 'any_name')
+  })
+})
