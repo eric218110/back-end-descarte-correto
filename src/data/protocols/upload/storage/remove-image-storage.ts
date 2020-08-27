360104997f0009c2a4d7be73b9eef9fbbcb01b26
Eric Silva Mendes <ericsilvaccp@gmail.com>
2020-08-27 14:06:21 -0300

    feat: ensure RemoveImageStorage call RemovedImageStorage with correct values

---
+++ src/data/protocols/upload/storage/remove-image-storage.ts
@@ -0,0 +1,3 @@
+export interface RemovedImageStorage {
+  removeImage: (request: any) => Promise<void>
+}
---
+++ src/data/usecases/upload/storage/remove/remove-image-storage.ts
@@ -0,0 +1,12 @@
+import { StorageRemoveFile } from '@domain/usecases/upload/storage/storage-remove-file'
+import { RemovedImageStorage } from '@data/protocols/upload/storage/remove-image-storage'
+
+export class RemoveImageStorage implements StorageRemoveFile {
+  constructor (
+    private readonly removedImageStorage: RemovedImageStorage
+  ) {}
+
+  async remove (pathFile: string): Promise<void> {
+    await this.removedImageStorage.removeImage(pathFile)
+  }
+}
