2755e128d076b1c418a9897102fc4c6b9df9a43c
Eric Silva Mendes <ericsilvaccp@gmail.com>
2020-08-28 14:31:03 -0300

    feat: ensure StorageTypeAwsAdapter throws if file not exists in request

--- src/infra/upload/storage/aws/storage-type-aws-adapter.ts
+++ src/infra/upload/storage/aws/storage-type-aws-adapter.ts
@@ -1,7 +1,19 @@
 import { SavedImageStorage } from '@data/protocols/upload/storage/saved-image-storage'
-
 export class StorageTypeAwsAdapter implements SavedImageStorage {
+  constructor (
+    private readonly config: {
+      AWS_ACCESS_KEY_ID: string
+      AWS_SECRET_ACCESS_KEY: string
+      AWS_DEFAULT_REGION: string
+      AWS_BUCKET: string
+      AWS_ACL: string
+    }
+  ) {}
+
   async saveImage (request: any): Promise<string> {
+    if (request.file === undefined) {
+      throw TypeError('file is required')
+    }
     return new Promise(resolve => resolve('htt://any_ur.com'))
   }
 }
