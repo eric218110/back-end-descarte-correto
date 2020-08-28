bd1425834f15460721e3f61bffb2cc1f9c1806d6
Eric Silva Mendes <ericsilvaccp@gmail.com>
2020-08-28 18:10:48 -0300

    refactor: separete tests in describe blocks

--- src/infra/upload/storage/aws/storage-type-aws-adapter.spec.ts
+++ src/infra/upload/storage/aws/storage-type-aws-adapter.spec.ts
@@ -43,59 +43,61 @@ afterAll(async () => {
 })
 
 describe('StorageTypeAwsAdapter', () => {
-  test('should return throws file not exist in request', async () => {
-    const { sut } = makeSut()
-    const fileUrl = sut.saveImage({
-      body: {
-        pathFile: 'any_path'
-      }
+  describe('Upload()', () => {
+    test('should return throws file not exist in request', async () => {
+      const { sut } = makeSut()
+      const fileUrl = sut.saveImage({
+        body: {
+          pathFile: 'any_path'
+        }
+      })
+      await expect(fileUrl).rejects.toThrow(TypeError('file is required'))
     })
-    await expect(fileUrl).rejects.toThrow(TypeError('file is required'))
-  })
 
-  test('should return throws request not exist in request', async () => {
-    const { sut } = makeSut()
-    const fileUrl = sut.saveImage(undefined)
-    await expect(fileUrl).rejects.toThrow(TypeError('request is required'))
-  })
-
-  test('should return throws path file is invalid', async () => {
-    const { sut } = makeSut()
-    const fileUrl = sut.saveImage({
-      body: {
-        pathFile: 'invalid_path'
-      },
-      file: {
-        destination: 'invalid_destination',
-        encoding: 'invalid_encoding',
-        fieldname: 'invalid_fieldname',
-        filename: 'valid_invalid_filename.jpg',
-        mimetype: 'invalid_mimetype',
-        originalname: 'invalid_originalname',
-        path: 'invalid_path',
-        size: 'invalid_size',
-        stream: 'invalid_stream'
-      }
+    test('should return throws request not exist in request', async () => {
+      const { sut } = makeSut()
+      const fileUrl = sut.saveImage(undefined)
+      await expect(fileUrl).rejects.toThrow(TypeError('request is required'))
     })
-    await expect(fileUrl).rejects.toThrow(TypeError('file path is undefined'))
-  })
 
-  test('should return throws if AWS config incorrect', async () => {
-    const sut = new StorageTypeAwsAdapter({
-      AWS_ACCESS_KEY_ID: 'invalid_key',
-      AWS_SECRET_ACCESS_KEY: '99vhrVY9sSvVN0ktfeIvEzglIpISl6iJZ06F9YdD',
-      AWS_DEFAULT_REGION: 'us-east-1',
-      AWS_BUCKET: 'tem-coleta-back-end-test',
-      AWS_ACL: 'public-read'
+    test('should return throws path file is invalid', async () => {
+      const { sut } = makeSut()
+      const fileUrl = sut.saveImage({
+        body: {
+          pathFile: 'invalid_path'
+        },
+        file: {
+          destination: 'invalid_destination',
+          encoding: 'invalid_encoding',
+          fieldname: 'invalid_fieldname',
+          filename: 'valid_invalid_filename.jpg',
+          mimetype: 'invalid_mimetype',
+          originalname: 'invalid_originalname',
+          path: 'invalid_path',
+          size: 'invalid_size',
+          stream: 'invalid_stream'
+        }
+      })
+      await expect(fileUrl).rejects.toThrow(TypeError('file path is undefined'))
     })
-    const fileUrl = sut.saveImage(makeFileRequestFake())
-    await expect(fileUrl).rejects.toThrow(TypeError('Error invalid config'))
-  })
 
-  test('should return image url if image save in AWS S3', async () => {
-    const { sut } = makeSut()
-    const fileUrl = await sut.saveImage(makeFileRequestFake())
-    const uploadSuccess = fileUrl.indexOf('file-test-aws.png')
-    expect(uploadSuccess).toBeTruthy()
+    test('should return throws if AWS config incorrect', async () => {
+      const sut = new StorageTypeAwsAdapter({
+        AWS_ACCESS_KEY_ID: 'invalid_key',
+        AWS_SECRET_ACCESS_KEY: '99vhrVY9sSvVN0ktfeIvEzglIpISl6iJZ06F9YdD',
+        AWS_DEFAULT_REGION: 'us-east-1',
+        AWS_BUCKET: 'tem-coleta-back-end-test',
+        AWS_ACL: 'public-read'
+      })
+      const fileUrl = sut.saveImage(makeFileRequestFake())
+      await expect(fileUrl).rejects.toThrow(TypeError('Error invalid config'))
+    })
+
+    test('should return image url if image save in AWS S3', async () => {
+      const { sut } = makeSut()
+      const fileUrl = await sut.saveImage(makeFileRequestFake())
+      const uploadSuccess = fileUrl.indexOf('file-test-aws.png')
+      expect(uploadSuccess).toBeTruthy()
+    })
   })
 })
