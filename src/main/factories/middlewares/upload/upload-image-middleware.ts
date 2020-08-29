52a25e92f7437d0599808b9af612877d51fd50ef
Eric Silva Mendes <ericsilvaccp@gmail.com>
2020-08-28 23:30:23 -0300

    refactor: use factories

--- src/main/factories/middlewares/upload/upload-image-middleware.ts
+++ src/main/factories/middlewares/upload/upload-image-middleware.ts
@@ -1,13 +1,7 @@
 import { Middleware } from '@presentation/protocols'
 import { UploadImageMiddleware } from '@presentation/middlewares/upload/upload-image-middleware'
-import { SaveImage } from '@data/usecases/upload/upload-image/save-image'
-import { MulterAdapter } from '@infra/upload/multer-adapter/multer-adapter'
-import env from '@main/config/env'
-import { StorageTypeLocalAdapter } from '@infra/upload/storage/local/storage-type-local-adapter'
+import { makeSaveImage } from '@main/factories/usecases/upload/storage/save/make-save-image-storage'
 
 export const makeFileImageUploadMiddleware = (): Middleware => {
-  const multerAdapter = new MulterAdapter()
-  const storageType = new StorageTypeLocalAdapter(env.HOST_STATIC_PATH)
-  const uploadImage = new SaveImage(multerAdapter, storageType)
-  return new UploadImageMiddleware(uploadImage)
+  return new UploadImageMiddleware(makeSaveImage())
 }
