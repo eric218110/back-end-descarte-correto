39af436ad09c542b96a88efe6d0b5f3aa3a557c8
Eric Silva Mendes <ericsilvaccp@gmail.com>
2020-08-29 16:20:00 -0300

    refactor: add mode in log

--- src/main/server/index.ts
+++ src/main/server/index.ts
@@ -1,9 +1,9 @@
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import env from '../config/env'
 
-const { MONGO_DB_URL, PORT } = env
+const { MONGO_DB_URL, PORT, MODE } = env
 
 MongoHelper.connect(MONGO_DB_URL).then(async () => {
   const app = (await import('../config/app')).default
-  app.listen(PORT, () => console.log(`Server running in port ${PORT}`))
+  app.listen(PORT, () => console.log(`Server running in port ${PORT} - MODE: ${MODE}`))
 }).catch(error => console.log(error))
