74786c2e0bf6af13399609ad72748fe2fc7972bd
Eric Silva Mendes <ericsilvaccp@gmail.com>
2020-08-29 16:09:44 -0300

    fix: await promisses resolve

--- src/infra/upload/multer-adapter/multer-adapter.ts
+++ src/infra/upload/multer-adapter/multer-adapter.ts
@@ -2,21 +2,21 @@ import multer from 'multer'
 import { ImageFileUploader, FileUploadProps } from '@data/protocols/upload/image-file-uploader'
 import { MulterHelper } from '@infra/upload/multer-adapter/helper/multer-helper'
 import { SavedImageStorage } from '@data/protocols/upload/storage/saved-image-storage'
+import { promisify } from 'util'
 export class MulterAdapter implements ImageFileUploader {
   async imageUpload (file: FileUploadProps, saveFileStorage: SavedImageStorage): Promise<void> {
     const { request, response } = file
     const upload = multer(MulterHelper.setConfig(multer)).single('file')
-    // eslint-disable-next-line @typescript-eslint/no-misused-promises
-    upload(request, response, async (errorMulter: any) => {
+    await promisify(upload)(request, response).then(async () => {
       try {
-        if (errorMulter) {
-          throw new Error(errorMulter.message)
-        }
         const imageUrl = await saveFileStorage.saveImage(request)
         request.body.file = imageUrl
       } catch (error) {
+        console.log(error)
         request.body.error = error.message
       }
+    }).catch((error) => {
+      request.body.error = error.message
     })
   }
 }
