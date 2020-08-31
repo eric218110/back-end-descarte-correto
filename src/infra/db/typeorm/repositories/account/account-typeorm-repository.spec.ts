2cb9a0935cc675b9c3208a2850a80881b4341490
Eric Silva Mendes <ericsilvaccp@gmail.com>
2020-08-31 20:24:03 -0300

    test: ensure AccountTypeOrmRepository return an account on loadByToken sucess

--- src/infra/db/typeorm/repositories/account/account-typeorm-repository.spec.ts
+++ src/infra/db/typeorm/repositories/account/account-typeorm-repository.spec.ts
@@ -1,6 +1,10 @@
 import { connectionDatabase } from '../../utils/create-connections'
 import { AddAccountModel } from '@domain/usecases/account/add-account'
 import { AccountTypeOrmRepository } from './account-typeorm-repository'
+import { Repository } from 'typeorm'
+import { EntityAccount } from '../../entities/account.entity'
+
+let accountTypeOrmRepository: Repository<EntityAccount>
 
 const makeFakeAddAccountModel = (): AddAccountModel => ({
   name: 'any_name',
@@ -24,8 +28,8 @@ const makeSut = (): SutTypes => {
 
 describe('AccountTypeOrmRepository', () => {
   beforeAll(async () => {
-    await connectionDatabase.create()
-    console.log('Connected database in mode: ' + process.env.MODE)
+    const connection = await connectionDatabase.create()
+    accountTypeOrmRepository = connection.getRepository(EntityAccount)
   })
 
   afterAll(async () => {
@@ -37,15 +41,36 @@ describe('AccountTypeOrmRepository', () => {
     await connectionDatabase.clear()
   })
 
-  describe('Add', () => {
-    test('should return an account on add sucess', async () => {
-      const { sut, fakeAddAccountModel } = makeSut()
-      const account = await sut.add(fakeAddAccountModel)
-      expect(account).toBeTruthy()
-      expect(account.id).toBeTruthy()
-      expect(account.name).toBe('any_name')
-      expect(account.email).toBe('any_email@mail.com')
-      expect(account.password).toBe('any_password')
+  describe('AccountTypeOrmRepository', () => {
+    describe('Add', () => {
+      test('should return an account on add sucess', async () => {
+        const { sut, fakeAddAccountModel } = makeSut()
+        const account = await sut.add(fakeAddAccountModel)
+        expect(account).toBeTruthy()
+        expect(account.id).toBeTruthy()
+        expect(account.name).toBe('any_name')
+        expect(account.email).toBe('any_email@mail.com')
+        expect(account.password).toBe('any_password')
+      })
+    })
+
+    describe('LoadAccountByToken', () => {
+      test('should return an account on loadByToken sucess', async () => {
+        const { sut, fakeAddAccountModel } = makeSut()
+        await accountTypeOrmRepository.insert({
+          id: 'any_id',
+          name: 'any_name',
+          email: 'any_email@mail.com',
+          password: 'any_password',
+          accessToken: 'any_token'
+        })
+        const account = await sut.loadByToken('any_token')
+        expect(account).toBeTruthy()
+        expect(account.id).toBeTruthy()
+        expect(account.name).toBe(fakeAddAccountModel.name)
+        expect(account.email).toBe(fakeAddAccountModel.email)
+        expect(account.password).toBe(fakeAddAccountModel.password)
+      })
     })
   })
 })
