824067a63cc6bbb816a67674d2acae4858328409
Eric Silva Mendes <ericsilvaccp@gmail.com>
2020-08-31 23:15:48 -0300

    feat: ensure AccountTypeOrmRepository update the account accessToken on UpdateAccessToken success

--- src/infra/db/typeorm/repositories/account/account-typeorm-repository.ts
+++ src/infra/db/typeorm/repositories/account/account-typeorm-repository.ts
@@ -4,11 +4,13 @@ import { Repository, getRepository } from 'typeorm'
 import { EntityAccount } from '../../entities/account.entity'
 import { LoadAccountByTokenRepository } from '@data/protocols/data/account/load-by-token-repository'
 import { LoadAccountByEmailRepository } from '@data/protocols/data/account/load-by-email-repository'
+import { UpdateAccessTokenRepository } from '@data/protocols/data/account/update-access-token-repository'
 
 export class AccountTypeOrmRepository implements
+LoadAccountByEmailRepository,
 AddAccountRepository,
 LoadAccountByTokenRepository,
-LoadAccountByEmailRepository {
+UpdateAccessTokenRepository {
   private readonly AccountTypeOrmRepository: Repository<EntityAccount>
 
   constructor () {
@@ -21,6 +23,10 @@ LoadAccountByEmailRepository {
     return createAccount
   }
 
+  async loadWithEmail (email: string): Promise<AccountModelData> {
+    return await this.AccountTypeOrmRepository.findOne({ email }) || null
+  }
+
   async loadByToken (token: string, role: string = 'user'): Promise<AccountModelData> {
     const account = await this.AccountTypeOrmRepository
       .createQueryBuilder('account')
@@ -31,7 +37,12 @@ LoadAccountByEmailRepository {
     return account || null
   }
 
-  async loadWithEmail (email: string): Promise<AccountModelData> {
-    return await this.AccountTypeOrmRepository.findOne({ email }) || null
+  async updateAccessToken (id: string, token: string): Promise<void> {
+    const account = await this.AccountTypeOrmRepository.findOne({ id })
+    if (account) {
+      const { id } = account
+      await this.AccountTypeOrmRepository.update({ id }, { accessToken: token })
+    }
+    return null
   }
 }
