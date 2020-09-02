dde1924eb0ec8ca189144d942504c71fce3985af
Eric Silva Mendes <ericsilvaccp@gmail.com>
2020-09-02 02:19:57 -0300

    refactor: remove comments

--- src/main/server/index.ts
+++ src/main/server/index.ts
@@ -1,21 +1,8 @@
-// import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import env from '../config/env'
 import { connectionDatabase } from '@infra/db/typeorm/utils/create-connections'
 
 const { PORT, MODE } = env
 
-// MongoHelper.connect(MONGO_DB_URL).then(async () => {
-//   const app = (await import('../config/app')).default
-//   app.listen(PORT, () => console.log(`Server running in port ${PORT} - MODE: ${MODE}`))
-// }).catch(error => console.log(error))
-
-// async function bootstrap (): Promise<void> {
-//   const connection = await connectionDatabase.create()
-//   const app = (await import('../config/app')).default
-//   app.listen(PORT, () => console.log(`Server running in port ${PORT} - MODE: ${MODE}`))
-// }
-
-// eslint-disable-next-line @typescript-eslint/no-floating-promises
 connectionDatabase.create().then(async () => {
   const app = (await import('../config/app')).default
   app.listen(PORT, () => console.log(`Server running in port ${PORT} - MODE: ${MODE}`))
