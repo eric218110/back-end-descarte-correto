c63d51d58fdb255f3709692b7a2c81ce19e6f3f8
Eric Silva Mendes <ericsilvaccp@gmail.com>
2020-09-03 19:35:07 -0300

    test: ensure PointTypeOrmRepository return null if Account not exists

--- src/infra/db/typeorm/repositories/point/point-typeorm-repository.spec.ts
+++ src/infra/db/typeorm/repositories/point/point-typeorm-repository.spec.ts
@@ -3,10 +3,8 @@ import { AddPointModelData } from '@data/models/point-model'
 import { PointTypeOrmRepository } from './point-typeorm-repository'
 import { Repository } from 'typeorm'
 import { EntityItem } from '../../entities/item.entity'
-import { EntityAccount } from '../../entities/account.entity'
 
 let itemTypeOrmRepository: Repository<EntityItem>
-let accountTypeOrmRepository: Repository<EntityAccount>
 
 interface SutTypes {
   sut: PointTypeOrmRepository
@@ -52,26 +50,13 @@ const makeFakeItems = async (): Promise<EntityItem[]> => {
   return [saveFirstItem, saveSecondItem]
 }
 
-const makeFakeAccount = async (): Promise<EntityAccount> => {
-  const createAccount = accountTypeOrmRepository.create({
-    name: 'any_name_account',
-    email: 'any_email_account',
-    password: 'any_password_account',
-    accessToken: 'any_accessToken_account',
-    role: 'any_role_account'
-  })
-  const saveAccount = accountTypeOrmRepository.save(createAccount)
-  return saveAccount
-}
-
 beforeAll(async () => {
   const connection = await connectionDatabase.create()
-  await connectionDatabase.clear()
   itemTypeOrmRepository = connection.getRepository(EntityItem)
-  accountTypeOrmRepository = connection.getRepository(EntityAccount)
 })
 
 afterAll(async () => {
+  await connectionDatabase.clear()
   await connectionDatabase.close()
 })
 
@@ -81,15 +66,12 @@ beforeEach(async () => {
 
 describe('PointTypeOrmRepository', () => {
   describe('Add', () => {
-    test('should add new Point', async () => {
+    test('should return null if account not exist in database', async () => {
       const { sut } = makeSut()
       const fakePoint = makeAddPointFake()
       fakePoint.items = await makeFakeItems()
-      fakePoint.account = await makeFakeAccount()
       const point = await sut.addNewPoint(fakePoint)
-      expect(point).toBeTruthy()
-      expect(point.id).toBeTruthy()
-      expect(point.name).toEqual(fakePoint.name)
+      expect(point).toBeNull()
     })
   })
 })
