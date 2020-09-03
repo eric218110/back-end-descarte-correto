548870fdb72f0b800862f30e55d72feeb8a10bb9
Eric Silva Mendes <ericsilvaccp@gmail.com>
2020-09-03 20:35:31 -0300

    test: ensure PointTypeOrmRepository return null if item no empty but items not exists in database

--- src/infra/db/typeorm/repositories/point/point-typeorm-repository.spec.ts
+++ src/infra/db/typeorm/repositories/point/point-typeorm-repository.spec.ts
@@ -3,8 +3,10 @@ import { AddPointModelData } from '@data/models/point-model'
 import { PointTypeOrmRepository } from './point-typeorm-repository'
 import { Repository } from 'typeorm'
 import { EntityItem } from '../../entities/item.entity'
+import { EntityAccount } from '../../entities/account.entity'
 
 let itemTypeOrmRepository: Repository<EntityItem>
+let accountTypeOrmRepository: Repository<EntityAccount>
 
 interface SutTypes {
   sut: PointTypeOrmRepository
@@ -50,13 +52,26 @@ const makeFakeItems = async (): Promise<EntityItem[]> => {
   return [saveFirstItem, saveSecondItem]
 }
 
+const makeFakeAccount = async (): Promise<EntityAccount> => {
+  const createAccount = accountTypeOrmRepository.create({
+    name: 'any_name_account',
+    email: 'any_email_account',
+    password: 'any_password_account',
+    accessToken: 'any_accessToken_account',
+    role: 'any_role_account'
+  })
+  const saveAccount = accountTypeOrmRepository.save(createAccount)
+  return saveAccount
+}
+
 beforeAll(async () => {
   const connection = await connectionDatabase.create()
+  await connectionDatabase.clear()
   itemTypeOrmRepository = connection.getRepository(EntityItem)
+  accountTypeOrmRepository = connection.getRepository(EntityAccount)
 })
 
 afterAll(async () => {
-  await connectionDatabase.clear()
   await connectionDatabase.close()
 })
 
@@ -70,6 +85,34 @@ describe('PointTypeOrmRepository', () => {
       const { sut } = makeSut()
       const fakePoint = makeAddPointFake()
       fakePoint.items = await makeFakeItems()
+      const point = await sut.addNewPoint(fakePoint)
+      expect(point).toBeNull()
+    })
+
+    test('should return null if item is empty', async () => {
+      const { sut } = makeSut()
+      const fakePoint = makeAddPointFake()
+      fakePoint.account = await makeFakeAccount()
+      const point = await sut.addNewPoint(fakePoint)
+      expect(point).toBeNull()
+    })
+
+    test('should return null if item no empty but items not exists in database', async () => {
+      const { sut } = makeSut()
+      const fakePoint = makeAddPointFake()
+      fakePoint.account = await makeFakeAccount()
+      fakePoint.items = [
+        {
+          id: 'first_invalid_id_item',
+          image: 'http://first_invalid_image_url.com',
+          title: 'First Invalid Image'
+        },
+        {
+          id: 'seconf_invalid_id_item',
+          image: 'http://seconf_invalid_image_url.com',
+          title: 'Seconf Invalid Image'
+        }
+      ]
       const point = await sut.addNewPoint(fakePoint)
       expect(point).toBeNull()
     })
