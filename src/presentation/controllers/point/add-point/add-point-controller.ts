16b7e09401578b8b27e265a08d215e9acc9a0f42
Eric Silva Mendes <ericsilvaccp@gmail.com>
2020-09-04 03:27:58 -0300

    feat: ensure AddPointController return 500 if Validator throws

--- src/presentation/controllers/point/add-point/add-point-controller.ts
+++ src/presentation/controllers/point/add-point/add-point-controller.ts
@@ -6,7 +6,11 @@ import {
 } from '@presentation/protocols'
 import { AddPoint } from '@domain/usecases/point/add-point'
 import { LoadItemByIds } from '@data/protocols/data/items/load-items-by-ids'
-import { badRequest, forbidden } from '@presentation/helper/http/http-helper'
+import {
+  badRequest,
+  forbidden,
+  serverError
+} from '@presentation/helper/http/http-helper'
 import { ItemNotExistError } from '@presentation/errors'
 import { AccessDeniedError } from '@presentation/errors/access-denied-error'
 import { LoadAccountByToken } from '@domain/usecases/account/load-accout-by-token'
@@ -20,22 +24,26 @@ export class AddPointController implements Controller {
   ) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { items, accountId } = httpRequest.body
-    const itemExist = await this.loadItemByIds.loadItems(
-      items.map(({ item }) => item)
-    )
-    if (!itemExist) return badRequest(new ItemNotExistError())
+    try {
+      const { items, accountId } = httpRequest.body
+      const itemExist = await this.loadItemByIds.loadItems(
+        items.map(({ item }) => item)
+      )
+      if (!itemExist) return badRequest(new ItemNotExistError())
 
-    if (!accountId) return forbidden(new AccessDeniedError())
+      if (!accountId) return forbidden(new AccessDeniedError())
 
-    const account = await this.loadAccountByToken.load(accountId)
-    if (!account) return forbidden(new AccessDeniedError())
+      const account = await this.loadAccountByToken.load(accountId)
+      if (!account) return forbidden(new AccessDeniedError())
 
-    const isError = this.validator.isValid(httpRequest.body)
-    if (isError) {
-      return badRequest(isError)
+      const isError = this.validator.isValid(httpRequest.body)
+      if (isError) {
+        return badRequest(isError)
+      }
+
+      return null
+    } catch (error) {
+      return serverError(error)
     }
-
-    return null
   }
 }
