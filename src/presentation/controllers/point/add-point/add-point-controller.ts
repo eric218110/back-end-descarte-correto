7597396eef46213a1d82129d70810f60724b0567
Eric Silva Mendes <ericsilvaccp@gmail.com>
2020-09-04 03:13:49 -0300

    feat: ensure AddPointController call LoadAccountByToken with correct values

--- src/presentation/controllers/point/add-point/add-point-controller.ts
+++ src/presentation/controllers/point/add-point/add-point-controller.ts
@@ -4,50 +4,26 @@ import { LoadItemByIds } from '@data/protocols/data/items/load-items-by-ids'
 import { badRequest, forbidden } from '@presentation/helper/http/http-helper'
 import { ItemNotExistError } from '@presentation/errors'
 import { AccessDeniedError } from '@presentation/errors/access-denied-error'
+import { LoadAccountByToken } from '@domain/usecases/account/load-accout-by-token'
 
 export class AddPointController implements Controller {
   constructor(
     private readonly addPoint: AddPoint,
-    private readonly loadItemByIds: LoadItemByIds
+    private readonly loadItemByIds: LoadItemByIds,
+    private readonly loadAccountByToken: LoadAccountByToken
   ) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { items } = httpRequest.body
+    const { items, accountId } = httpRequest.body
     const itemExist = await this.loadItemByIds.loadItems(
       items.map(({ item }) => item)
     )
     if (!itemExist) return badRequest(new ItemNotExistError())
-    const { accountId } = httpRequest.body
+
     if (!accountId) return forbidden(new AccessDeniedError())
-    await this.addPoint.add({
-      name: 'any_name',
-      city: 'any_city',
-      image: 'any_image_url',
-      latitude: 'any_latitude',
-      longitude: 'any_longitude',
-      state: 'any_state',
-      phone: 'any_phone',
-      account: {
-        id: 'valid_id_user',
-        name: 'valid_name',
-        email: 'valid_email_user',
-        password: 'valid_password',
-        accessToken: 'valid_access_token',
-        role: 'valid_role'
-      },
-      items: [
-        {
-          id: 'valid_id_item_1',
-          image: 'http://valid_item_image_1_url.com.br',
-          title: 'valid_item_image_1'
-        },
-        {
-          id: 'valid_id_item_2',
-          image: 'http://valid_item_image_2_url.com.br',
-          title: 'valid_item_image_2'
-        }
-      ]
-    })
+
+    await this.loadAccountByToken.load(accountId)
+
     return null
   }
 }
