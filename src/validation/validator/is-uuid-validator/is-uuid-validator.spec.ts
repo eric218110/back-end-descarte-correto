28d46bb30e847462628f6f2c51be5cf66d199c17
Eric Silva Mendes <ericsilvaccp@gmail.com>
2020-09-05 07:32:34 -0300

    feat: ensure IsUuidValidtor call uuid validator with correct accountId

---
+++ src/validation/validator/is-uuid-validator/is-uuid-validator.spec.ts
@@ -0,0 +1,42 @@
+import { IsUuidValidatorComposite } from './is-uuid-validator'
+import { IsUuidValidator } from '@validation/protocols/is-uuid-validator'
+import { InvalidParamError } from '@presentation/errors'
+
+const makeIsUuidValidator = (): IsUuidValidator => {
+  class IsUuidValidatorStub implements IsUuidValidator {
+    isValid(accountId: string): boolean {
+      return true
+    }
+  }
+  return new IsUuidValidatorStub()
+}
+
+type ISutTypes = {
+  isUuidValidatorStub: IsUuidValidator
+  sut: IsUuidValidatorComposite
+}
+
+const makeSut = (): ISutTypes => {
+  const isUuidValidatorStub = makeIsUuidValidator()
+  const sut = new IsUuidValidatorComposite('accountId', isUuidValidatorStub)
+  return {
+    sut,
+    isUuidValidatorStub
+  }
+}
+
+describe('UUID Validator Composite', () => {
+  test('Should return an error if UuidValidator returns false', () => {
+    const { sut, isUuidValidatorStub } = makeSut()
+    jest.spyOn(isUuidValidatorStub, 'isValid').mockReturnValueOnce(false)
+    const error = sut.isValid({ accountId: 'any_account_id' })
+    expect(error).toEqual(new InvalidParamError('accountId'))
+  })
+
+  test('Should call uuid validator with correct accountID', () => {
+    const { sut, isUuidValidatorStub } = makeSut()
+    const isValidSpy = jest.spyOn(isUuidValidatorStub, 'isValid')
+    sut.isValid({ accountId: 'any_account_id' })
+    expect(isValidSpy).toHaveBeenCalledWith('any_account_id')
+  })
+})
