606d026f01a9482ca362ac22a28cd584646b0b83
Eric Silva Mendes <ericsilvaccp@gmail.com>
2020-09-05 03:23:37 -0300

    test: ensure end point /api/point return error if Storage retrun error

--- src/main/routes/point.test.ts
+++ src/main/routes/point.test.ts
@@ -85,5 +85,23 @@ describe('POST SignUp Route', () => {
         .attach('file', resolve('__test__', 'file', 'no-supported-test.txt'))
         .expect(400)
     })
+
+    test('Should error if Storage retrun error', async () => {
+      const account = accountTypeOrmRepository.create({
+        name: 'any_name',
+        email: 'any_email@email.com',
+        password: await hash('123', 12),
+        role: 'admin'
+      })
+      const result = await accountTypeOrmRepository.save(account)
+      const { id } = result
+      const accessToken = sign({ id }, env.JWT_SECRET)
+      await accountTypeOrmRepository.update({ id }, { accessToken })
+      await request(app())
+        .post('/api/item')
+        .set('x-access-token', accessToken)
+        .field({ title: 'any_title' })
+        .expect(400)
+    })
   })
 })
