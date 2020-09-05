3100bf08191bed2e15b45fb328a8cd515aac33bb
Eric Silva Mendes <ericsilvaccp@gmail.com>
2020-09-05 17:14:23 -0300

    feat: ensure LoadPointByIdController call validator with correct values

--- src/presentation/controllers/point/load-point-by-id/load-point-by-id-controller.ts
+++ src/presentation/controllers/point/load-point-by-id/load-point-by-id-controller.ts
@@ -1,11 +1,23 @@
 import { HttpRequest, HttpResponse } from '@presentation/protocols/http'
 import { Controller } from '@presentation/protocols/controller'
 import { LoadPointById } from '@domain/usecases/point/load-point-by-id'
+import {
+  Validator,
+  badRequest
+} from '../add-point/add-point-controller-protocols'
 
 export class LoadPointByIdController implements Controller {
-  constructor(private readonly loadPointById: LoadPointById) {}
+  constructor(
+    private readonly loadPointById: LoadPointById,
+    private readonly validator: Validator
+  ) {}
+
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    const isError = this.validator.isValid(httpRequest.params)
+    if (isError) {
+      return badRequest(isError)
+    }
     await this.loadPointById.load(httpRequest.params.id)
-    return new Promise(resolve => resolve(null))
+    return null
   }
 }
