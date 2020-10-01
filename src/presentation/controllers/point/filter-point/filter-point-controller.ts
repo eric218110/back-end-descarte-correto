8a8d69fa34b046cd2f51c5e1c73f49c32d7d212d
Eric Silva Mendes <ericsilvaccp@gmail.com>
2020-10-01 17:20:49 -0300

    test: ensure FilterPointController return 500 if FilterPoint throws

--- src/presentation/controllers/point/filter-point/filter-point-controller.ts
+++ src/presentation/controllers/point/filter-point/filter-point-controller.ts
@@ -1,15 +1,19 @@
 import { FilterPoint } from '@domain/usecases/point/filter-point'
 import { Controller } from '@presentation/protocols/controller'
 import { HttpRequest, HttpResponse } from '@presentation/protocols/http'
+import { serverError } from '../add-point/add-point-controller-protocols'
 
 export class FilterPointController implements Controller {
   constructor(private readonly filterPoint: FilterPoint) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    // if (httpRequest.params)
-    const itemsParams: string = httpRequest.params.items
-    const itemsId = itemsParams.replace(' ', '').trim().split(',')
-    await this.filterPoint.filter(itemsId)
-    return new Promise(resolve => resolve(null))
+    try {
+      const itemsParams: string = httpRequest.params.items
+      const itemsId = itemsParams.replace(' ', '').trim().split(',')
+      await this.filterPoint.filter(itemsId)
+      return new Promise(resolve => resolve(null))
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
