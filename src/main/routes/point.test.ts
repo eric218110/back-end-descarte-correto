d42e706ac03cbef7ce598baabf279b8c3507d3bd
Eric Silva Mendes <ericsilvaccp@gmail.com>
2020-10-10 21:23:03 -0300

    test: ensure GET /api/points return 200 if lists item success

--- src/main/routes/point.test.ts
+++ src/main/routes/point.test.ts
@@ -210,15 +210,20 @@ describe('GET /api/points', () => {
 })
 
 describe('GET /api/points/filter', () => {
-  test('should return 400 if params not is invalid', async () => {
-    await clearTables()
-    await request(app()).get('/api/points/filter/invalid_params').expect(400)
-  })
-
   test('should return 204 if params not exist', async () => {
     await clearTables()
     await request(app())
       .get('/api/points/filter/a1d82888-6e1c-46ef-aa07-4cb65a6053a2')
       .expect(204)
+  })
+
+  test('should return 200 if items exist', async () => {
+    await clearTables()
+    const points = await makeFakePoint()
+    const firstPointsId = points.items[0].id
+    const secondPointsId = points.items[1].id
+    await request(app())
+      .get(`/api/points/filter/${firstPointsId},${secondPointsId}`)
+      .expect(200)
   })
 })
