b42ecf04e45a584edba4b84045116200f64e4826
Eric Silva Mendes <ericsilvaccp@gmail.com>
2020-10-10 21:21:43 -0300

    feat: ensure GET /api/points return 200 if lists item success

--- src/main/factories/controllers/point/filter-points/filter-points-validator-factory.ts
+++ src/main/factories/controllers/point/filter-points/filter-points-validator-factory.ts
@@ -1,18 +1,13 @@
 import {
   ValidatorComposite,
-  RequireFieldValidator,
-  IsUuidValidatorComposite
+  RequireFieldValidator
 } from '@validation/validator'
 import { Validator } from '@presentation/protocols'
-import { IsUuidValidatorAdapter } from '@infra/validator/uuid-validator/is-uuid-validator-adapter'
 
 export const makeFilterPointValidator = (): ValidatorComposite => {
   const validators: Validator[] = []
   for (const field of ['items']) {
     validators.push(new RequireFieldValidator(field))
-    validators.push(
-      new IsUuidValidatorComposite('items', new IsUuidValidatorAdapter())
-    )
   }
   return new ValidatorComposite(validators)
 }
